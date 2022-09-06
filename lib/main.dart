560636fa8cf2a61552c1f417a39427dba0dc53f3
robertbrice <rob.a.brice@gmail.com>
2022-09-06 11:16:13 +0100

    messy attempts at trying to clear the canvas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:math';
+
 import 'package:drawing_app/random_painter.dart';
 import 'package:drawing_app/data_points.dart';
+import 'package:drawing_app/array_data_point.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,9 +33,8 @@ class BlankEasel extends StatefulWidget {
   BlankEaselState createState() => BlankEaselState();
 }
 
-class BlankEaselState extends State<BlankEasel>
-    with AutomaticKeepAliveClientMixin<BlankEasel> {
-/*
+class BlankEaselState extends State<BlankEasel> {
+/* Animation didn't work as it runs at the frame rate of the device: 60 or 120 fps
   late AnimationController _controller;
   @override
   void initState() {
@@ -49,13 +51,13 @@ class BlankEaselState extends State<BlankEasel>
   }
 */
 
-  @override
-  bool get wantKeepAlive => true;
-
   late DateTime _initialTime;
-  // 1. declare a Timer
   late final Timer _timer;
   Duration _elapsed = Duration.zero;
+  var arrayData = ArrayData();
+  List<ArrayDataPoint> data = [];
+  int height = 0;
+  int width = 0;
 
   @override
   void initState() {
@@ -66,15 +68,20 @@ class BlankEaselState extends State<BlankEasel>
       // 3. get the current time
       final now = DateTime.now();
       // 4. update the state (elapsed time)
+      width = MediaQuery.of(context).size.width.floor();
+      height = MediaQuery.of(context).size.height.floor();
+
       setState(() {
         _elapsed = now.difference(_initialTime);
+        debugPrint(
+            "height = ${height.toString()}, width = ${width.toString()}");
+        data = arrayData.getRandomStarterArray(width, height);
       });
     });
   }
 
   @override
   void dispose() {
-    // 5. don't forget to cancel the timer
     _timer.cancel();
     super.dispose;
   }
@@ -82,13 +89,126 @@ class BlankEaselState extends State<BlankEasel>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[50],
+      backgroundColor: Colors.blue[50],
       body: SizedBox.expand(
         child: CustomPaint(
-          painter: RandomPainter(_elapsed),
+          painter: RandomPainter(_elapsed, data),
           willChange: true,
+          isComplex: true,
         ),
       ),
     );
+  }
+}
+
+class ArrayData {
+  int count = 0;
+  var randomNumberGenerator = Random();
+  List<ArrayDataPoint> data = [];
+  int numberOfGenerations = 0;
+
+  List<ArrayDataPoint> getArray(width, height) {
+    if (numberOfGenerations == 0) {
+      data = getRandomStarterArray(width, height);
+      debugPrint("here");
+      return data;
+    } else {
+      data = getNextGeneration(data, width, height);
+      return data;
+    }
+  }
+
+  List<ArrayDataPoint> getRandomStarterArray(width, height) {
+    for (var i = 0; i < width; i++) {
+      for (var j = 0; j < height; j++) {
+        data.add(ArrayDataPoint(i, j, randomNumberGenerator.nextBool()));
+      }
+    }
+    return data;
+  }
+
+  List<ArrayDataPoint> getNextGeneration(data, width, height) {
+    int w = width.round();
+
+    for (var i = 0; i < data.length; i++) {
+      List<ArrayDataPoint> localArray = [];
+      var trueCount = 0;
+
+      if (!(data.elementAt(i).x == 0 ||
+          data.elementAt(i).x == width - 1 ||
+          data.elementAt(i).y == 0 ||
+          data.elementAt(i).y == height - 1)) {
+        localArray.add(ArrayDataPoint(data.elementAt(i - w - 1).x,
+            data.elementAt(i - w - 1).y, data.elementAt(i - w - 1).value));
+        localArray.add(ArrayDataPoint(data.elementAt(i - w).x,
+            data.elementAt(i - w).y, data.elementAt(i - w).value));
+        localArray.add(ArrayDataPoint(data.elementAt(i - w + 1).x,
+            data.elementAt(i - w + 1).y, data.elementAt(i - w + 1).value));
+        localArray.add(ArrayDataPoint(data.elementAt(i - 1).x,
+            data.elementAt(i - 1).y, data.elementAt(i - 1).value));
+        localArray.add(ArrayDataPoint(
+            data.elementAt(i).x, data.elementAt(i).y, data.elementAt(i).value));
+        localArray.add(ArrayDataPoint(data.elementAt(i + 1).x,
+            data.elementAt(i + 1).y, data.elementAt(i + 1).value));
+        localArray.add(ArrayDataPoint(data.elementAt(i + w - 1).x,
+            data.elementAt(i + w - 1).y, data.elementAt(i + w - 1).value));
+        localArray.add(ArrayDataPoint(data.elementAt(i + w).x,
+            data.elementAt(i + w).y, data.elementAt(i + w).value));
+        localArray.add(ArrayDataPoint(data.elementAt(i + w + 1).x,
+            data.elementAt(i + w + 1).y, data.elementAt(i + w + 1).value));
+      }
+
+      for (var j = 0; j < localArray.length; j++) {
+        if (localArray.elementAt(j).value) {
+          trueCount++;
+        }
+      }
+
+      debugPrint("truecount is ${trueCount.toString()}");
+
+      switch (trueCount) {
+        case 0:
+          data.elementAt(i).value = false;
+          break;
+        case 1:
+          data.elementAt(i).value = false;
+          break;
+        case 2:
+          data.elementAt(i).value = false;
+          break;
+        case 3:
+          data.elementAt(i).value = true;
+          break;
+        case 4:
+          data.elementAt(i).value = false;
+          break;
+        case 5:
+          data.elementAt(i).value = false;
+          break;
+        case 6:
+          data.elementAt(i).value = false;
+          break;
+        case 7:
+          data.elementAt(i).value = false;
+          break;
+        case 8:
+          data.elementAt(i).value = false;
+          break;
+        default:
+          debugPrint('woops');
+      }
+    }
+    numberOfGenerations++;
+    return data;
+  }
+
+  int getNumberOfGenerations() {
+    return numberOfGenerations;
+  }
+
+  void incrementTest(c) {
+    for (var i = 0; i < 20; i++) {
+      debugPrint("increment test - the value is $i and ${(c++).toString()}");
+    }
   }
 }
--- lib/random_painter.dart
+++ lib/random_painter.dart
@@ -5,25 +5,75 @@ import 'package:drawing_app/data_points.dart';
 
 class RandomPainter extends CustomPainter {
   final Duration elapsed;
-  RandomPainter(this.elapsed);
   final arrayData = ArrayData();
-  List<ArrayDataPoint> data = [];
-  int count = 0;
+  List<ArrayDataPoint> data;
+  RandomPainter(this.elapsed, this.data);
+
+  //final _blendMode = BlendMode.clear;
 
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
-      ..color = Colors.redAccent
+      ..color = Colors.redAccent.withOpacity(.99)
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 5.0;
 
     debugPrint("the timer is : ${elapsed.inSeconds.toString()}");
 
+    debugPrint(
+        "size is height : ${size.height.toString()}, width : ${size.width.toString()}");
+
+/*
     if (arrayData.numberOfGenerations == 0) {
       data = arrayData.getRandomStarterArray(size);
     } else {
       data = arrayData.getNextGeneration(data, size);
     }
+
+
+   canvas.save();
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height),
+        paint..color = Colors.transparent.withOpacity(.99));
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height),
+        paint..blendMode = BlendMode.clear);
+    canvas.restore();
+
+
+    // let's pretend this rectangle is an image.
+    // in this case, we don't want anything erased from the image,
+    // and we also want the image to be drawn under the eraser.
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height),
+        Paint()..color = Colors.white);
+
+    // after having drawn our image, we start a new layer using saveLayer().
+    canvas.saveLayer(Rect.fromLTWH(0, 0, size.width, size.height), Paint());
+
+    // drawing the line that should be erased partially.
+    canvas.drawLine(
+        const Offset(100, 100),
+        const Offset(200, 200),
+        Paint()
+          ..color = Colors.black
+          ..strokeWidth = 5.0);
+
+    // erasing parts of the first line where intersected with this line.
+    canvas.drawLine(
+        const Offset(100, 200),
+        const Offset(200, 100),
+        Paint()
+          ..color = Colors.red
+          ..strokeWidth = 5.0
+          ..blendMode = _blendMode);
+
+    // first composite this layer and then draw it over the previously drawn layers.
+    canvas.restore();
+
+    debugPrint(
+        "number of items on the saveStack = ${canvas.getSaveCount().toString()}");
+
+*/
+
+    canvas.saveLayer(Rect.fromLTWH(0, 0, size.width, size.height), Paint());
 
     for (var i = 0; i < data.length; i++) {
       if (data.elementAt(i).value) {
@@ -31,13 +81,27 @@ class RandomPainter extends CustomPainter {
             Offset(
                 data.elementAt(i).x.toDouble(), data.elementAt(i).y.toDouble()),
             .5,
-            paint);
+            paint..color = Colors.redAccent);
       }
     }
-    count++;
-    debugPrint("count is ${count.toString()}");
+
+    canvas.restore();
+
+    /*
+      else {
+        canvas.drawCircle(
+            Offset(
+                data.elementAt(i).x.toDouble(), data.elementAt(i).y.toDouble()),
+            .5,
+            paint..color = Colors.white);
+      }
+    }
+
+*/
+
+    //canvas.saveLayer((null), paint..blendMode = BlendMode.difference);
   }
 
   @override
-  bool shouldRepaint(covariant RandomPainter oldDelegate) => true;
+  bool shouldRepaint(RandomPainter oldDelegate) => true;
 }
